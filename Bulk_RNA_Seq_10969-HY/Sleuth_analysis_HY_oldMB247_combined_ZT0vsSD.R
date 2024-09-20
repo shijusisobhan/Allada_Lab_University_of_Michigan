be1fdeca49bd1aea7e50b50efc6b104e6146c009
shiju sisobhan <shijusisobhan@gmail.com>
2024-09-20 15:01:37 -0400

    find the DEG in combined RNA seq

--- Bulk_RNA_Seq_10969-HY/Sleuth_analysis_HY_oldMB247_combined_ZT0vsSD.R
+++ Bulk_RNA_Seq_10969-HY/Sleuth_analysis_HY_oldMB247_combined_ZT0vsSD.R
@@ -72,3 +72,70 @@ plot_pca(so, color_by = 'Batch', text_labels = F, units = "tpm")
 plot_pca(so, color_by = 'condition', text_labels = F, units = "tpm")
 
 
+# 1.How do we know what models are available for testing?
+models(so)
+
+beta='conditionSD'
+
+so <- sleuth_wt(so, which_beta = beta)
+
+sig_level=0.05
+sig_th =0.6
+test_table <- sleuth_results(so, beta)
+test_table$raw_b <- exp(test_table$b)
+test_table$log2_b <- log2(test_table$raw_b)
+test_table$neg_log10_qval<- -log10(test_table$qval)
+
+
+test_table$diffexpressed <- "Not sig"
+# if log2Foldchange > 0 and qvalue < 0.1, set as "UP" 
+test_table$diffexpressed[test_table$log2_b > sig_th  & test_table$qval < sig_level] <- "UP"
+# if log2Foldchange < -0.6 and pvalue < 0.05, set as "DOWN"
+test_table$diffexpressed[test_table$log2_b < -sig_th  & test_table$qval < sig_level] <- "DOWN"
+
+
+N_significant<-length(test_table$diffexpressed[test_table$diffexpressed !="Not sig"])
+N_UP<-length(test_table$diffexpressed[test_table$diffexpressed =="UP"])
+N_DOWN<-length(test_table$diffexpressed[test_table$diffexpressed =="DOWN"])
+
+N_significant
+N_UP
+N_DOWN
+
+
+
+
+top_genes<- test_table[1:5, "target_id"]
+
+library('ggplot2')
+library(ggrepel)
+
+ggplot(test_table) + geom_point(aes(x = log2_b, y = neg_log10_qval, col=diffexpressed))+
+  geom_vline(xintercept=0, col="black",  linetype="dashed")+
+  scale_color_manual(values=c("green","black", "red"))+
+  labs(x = "log2(FC)", y="-log10(q)", colour="DEG") +
+  geom_text_repel(data = subset(test_table, target_id == top_genes), 
+                  aes(x = log2_b, y = neg_log10_qval, label = target_id), color = "black")
+
+
+
+
+# get the gene expression table
+
+sleuth_gene_matrix <- sleuth_to_matrix(so, 'obs_norm', 'tpm')
+
+DGE_table<-cbind(row.names(sleuth_gene_matrix), sleuth_gene_matrix)# Add row names as one colum (gene names
+
+p_val_sort<-(test_table[order(test_table$target_id),]) # sort according to gene name
+
+
+p_val_reduce<-p_val_sort[c("target_id","pval","qval", "log2_b")] # extract only genename,p and q and log2()
+
+
+DGE_all<-merge(DGE_table, p_val_reduce, by.x=1 , by.y="target_id") # megrge with gene expression and p, q values
+
+DGE_all<-na.omit(DGE_all)
+
+
+write.csv(DGE_all, 'DEG_Combined_10969-HY_oldMB247_ZT0vsSD.csv')
+
